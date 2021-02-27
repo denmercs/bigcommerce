e971e0b2c35fa2d5f26ca8d9223f4f9782da54d3
denmercs <denmercs@gmail.com>
2021-02-26 23:57:48 -0600

    cant solved it

--- assets/js/theme/custom/special-items.js
+++ assets/js/theme/custom/special-items.js
@@ -2,17 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import PageManager from "../page-manager";
 
-const SpecialItem = (props) => (
+const SpecialItem = ({ context }) => (
   <div>
     <p>Hello World from React!</p>
     <p>Here are my props:</p>
-    <pre>{JSON.stringify(props, null, 2)}</pre>
+    <pre>{JSON.stringify({ context }, null, 2)}</pre>
+    {/* {context && <img src={context.category.products[0].image.data} />} */}
   </div>
 );
 
 export default class ReactDemo extends PageManager {
   onReady() {
     const container = document.querySelector("#specialItem");
+    console.log("this is the props", this.context);
     ReactDOM.render(<SpecialItem context={this.context} />, container);
   }
 }
